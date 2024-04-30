e0b8627c03c81935093be22845894b649a8e8077
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-04-30 18:45:13 -0300

    Add folder app

---
+++ app/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <form action="">
+    <input type="email" name="" id="">
+    <input type="password" name="" id="">
+
+    <input type="submit" value="Entrar">
+  </form>
+  
+</body>
+</html>
