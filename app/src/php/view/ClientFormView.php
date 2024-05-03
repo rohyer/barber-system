acf539afc4956c3aeaf96dbc1093dcc6fc333997
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-03 19:53:13 -0300

    Create ClientFormView

---
+++ app/src/php/view/ClientFormView.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <form action="cadastro-cliente" method="post">
+    <label for="name">Nome:</label>
+    <input type="text" id="name" name="name">
+
+    <label for="sex">Sexo:</label>
+    <input type="text" id="sex" name="sex">
+
+    <label for="address">Endereço:</label>
+    <input type="text" id="address" name="address">
+
+    <label for="birth">Data de Nascimento:</label>
+    <input type="text" id="birth" name="birth">
+
+    <label for="phone">Telefone:</label>
+    <input type="text" id="phone" name="phone">
+
+    <input type="submit" value="Cadastrar">
+  </form>
+
+</body>
+</html>
