7719608bd8d1e85f3f772cc2c05944b23efb5273
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-03 19:51:09 -0300

    Create ConnectionModel

---
+++ app/src/php/model/ConnectionModel.php
@@ -0,0 +1,23 @@
+<?php
+
+class ConnectionModel {
+  private $dsn;
+  private $user;
+  private $pass;
+
+  public function __construct() {
+    $this->dsn = "mysql:host=localhost;dbname=barbersystem";
+    $this->user = "root";
+    $this->pass = "";
+  }
+
+  public function getConnection() {
+    try {
+      $pdo = new PDO($this->dsn, $this->user, $this->pass);
+      return $pdo;
+    } catch(PDOException $error) {
+      echo "Error: " . $error->getMessage();
+    }
+  }
+
+}
