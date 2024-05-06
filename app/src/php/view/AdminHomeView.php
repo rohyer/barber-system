58bf02d520fab6ad18caf34b168dec86d2bb139c
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-05 23:52:00 -0300

    Create AdminHomeView to use on page home

---
+++ app/src/php/view/AdminHomeView.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+  <title>Dashboard</title>
+  <script src="https://kit.fontawesome.com/5d76c62972.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="../dist/css/style.min.css">
+</head>
+<body>
+  
+  <nav class="nav">
+    <div class="nav__top">
+      <a href=""><i class="nav__logo-icon fa-solid fa-scissors"></i></a>
+      <i class="nav__menu-icon fa-solid fa-bars"></i>
+    </div>
+
+    <ul>
+      <li>
+        <a href="" class="nav__link"><i class="fa-solid fa-house"></i>Dashboard</a>
+      </li>
+      <li>
+        <a href="" class="nav__link"><i class="fa-solid fa-calendar-days"></i>Agenda</a>
+      </li>
+      <li>
+        <a href="" class="nav__link"><i class="fa-solid fa-user-group"></i>Clientes</a>
+      </li>
+      <li>
+        <a href="" class="nav__link"><i class="fa-solid fa-handshake"></i>Colaboradores</a>
+      </li>
+    </ul>
+
+    <a href="" class="nav__logout"><i class="fa-solid fa-right-from-bracket"></i>Sair</a>
+  </nav>
+
+</body>
+</html>
