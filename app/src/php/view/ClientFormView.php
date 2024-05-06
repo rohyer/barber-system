b71bb0bcd77f6b64d2cd20dcb96d5dc7df5d43b4
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-06 13:54:35 -0300

    add navbar component

--- app/src/php/view/ClientFormView.php
+++ app/src/php/view/ClientFormView.php
@@ -6,6 +6,8 @@
   <title>Document</title>
 </head>
 <body>
+
+  <?php include dirname(__DIR__) . "/view/components/nav.php"; ?>
   
   <form action="cadastro-cliente" method="post">
     <label for="name">Nome:</label>
