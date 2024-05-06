d3e65707e9332f30f1c29d0053a6054169c12890
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-06 13:54:07 -0300

    Create new route to Employee

--- app/public/index.php
+++ app/public/index.php
@@ -2,6 +2,7 @@
 
 require dirname(__DIR__) . "/src/php/controller/ClientController.php";
 require dirname(__DIR__) . "/src/php/controller/AdminController.php";
+require dirname(__DIR__) . "/src/php/controller/EmployeeController.php";
 
 $uri = $_SERVER["REQUEST_URI"];
 
@@ -23,6 +24,10 @@ switch ($uri) {
 
   case '/barbersystem/app/public/clientes':
     ClientController::readClient();
+    break;
+
+  case '/barbersystem/app/public/colaboradores':
+    EmployeeController::readEmployee();
   
   default:
     # code...
