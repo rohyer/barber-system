6a37db61cf03a62d4540bb6fd4fac4714c3f6057
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-06 13:56:08 -0300

    Create nav

---
+++ app/src/php/view/components/nav.php
@@ -0,0 +1,23 @@
+<nav class="nav">
+  <div class="nav__top">
+    <a href=""><i class="nav__logo-icon fa-solid fa-scissors"></i></a>
+    <i class="nav__menu-icon fa-solid fa-bars"></i>
+  </div>
+
+  <ul>
+    <li>
+      <a href="/barbersystem/app/public" class="nav__link"><i class="fa-solid fa-house"></i>Dashboard</a>
+    </li>
+    <li>
+      <a href="agenda" class="nav__link"><i class="fa-solid fa-calendar-days"></i>Agenda</a>
+    </li>
+    <li>
+      <a href="clientes" class="nav__link"><i class="fa-solid fa-user-group"></i>Clientes</a>
+    </li>
+    <li>
+      <a href="colaboradores" class="nav__link"><i class="fa-solid fa-handshake"></i>Colaboradores</a>
+    </li>
+  </ul>
+
+  <a href="" class="nav__logout"><i class="fa-solid fa-right-from-bracket"></i>Sair</a>
+</nav>
