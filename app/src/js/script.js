20d04563b30f992029e0f9518e17f68ee81a52b4
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-08 12:07:45 -0300

    Develop code to collapse nav

---
+++ app/src/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", getStateMenu);
+
+function getStateMenu() {
+  const menuState = sessionStorage.getItem("menu");
+
+  if (menuState && menuState === "open") {
+    const nav = document.querySelector("nav.nav");
+  
+    nav.setAttribute("data-state", "open");    
+  }
+}
+
+const navMenu = document.getElementById("nav-menu");
+
+navMenu.addEventListener("click", changeMenuState);
+
+function changeMenuState() {
+  const nav = document.querySelector("nav.nav");
+  const navAttribute = nav.getAttribute("data-state");
+
+  if (navAttribute === "open") {
+    nav.setAttribute("data-state", "close");
+    sessionStorage.setItem("menu", "close");
+  } else if (navAttribute === "close") {
+    nav.setAttribute("data-state", "open");
+    sessionStorage.setItem("menu", "open");
+  }
+}
--- app/src/php/view/components/nav.php
+++ app/src/php/view/components/nav.php
@@ -1,23 +1,35 @@
-<nav class="nav">
+<nav class="nav" data-state="close">
   <div class="nav__top">
-    <a href=""><i class="nav__logo-icon fa-solid fa-scissors"></i></a>
-    <i class="nav__menu-icon fa-solid fa-bars"></i>
+    <!-- <a href=""><i class="nav__logo-icon fa-solid fa-scissors"></i></a> -->
+    <div><i class="nav__menu-icon fa-solid fa-bars" id="nav-menu"></i></div>
   </div>
 
   <ul>
     <li>
-      <a href="/barbersystem/app/public" class="nav__link"><i class="fa-solid fa-house"></i>Dashboard</a>
+      <a href="/barbersystem/app/public" class="nav__link">
+        <div><i class="fa-solid fa-house"></i></div>Dashboard
+      </a>
     </li>
     <li>
-      <a href="agenda" class="nav__link"><i class="fa-solid fa-calendar-days"></i>Agenda</a>
+      <a href="agenda" class="nav__link">
+        <div><i class="fa-solid fa-calendar-days"></i></div>Agenda
+      </a>
     </li>
     <li>
-      <a href="clientes" class="nav__link"><i class="fa-solid fa-user-group"></i>Clientes</a>
+      <a href="clientes" class="nav__link">
+        <div><i class="fa-solid fa-user-group"></i></div>Clientes
+      </a>
     </li>
     <li>
-      <a href="colaboradores" class="nav__link"><i class="fa-solid fa-handshake"></i>Colaboradores</a>
+      <a href="colaboradores" class="nav__link">
+        <div><i class="fa-solid fa-handshake"></i></div>Colaboradores
+      </a>
     </li>
   </ul>
 
-  <a href="" class="nav__logout"><i class="fa-solid fa-right-from-bracket"></i>Sair</a>
+  <a href="" class="nav__logout">
+    <div><i class="fa-solid fa-right-from-bracket"></i></div>Sair
+  </a>
 </nav>
+
+<script type="module" src="../dist/js/all.js"></script>
