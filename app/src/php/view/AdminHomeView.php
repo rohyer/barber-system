4299f618ca45ce2459cfa622da01efa8d1b35367
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-07 23:35:34 -0300

    Add nav to specific file

--- app/src/php/view/AdminHomeView.php
+++ app/src/php/view/AdminHomeView.php
@@ -12,29 +12,7 @@
 </head>
 <body>
   
-  <nav class="nav">
-    <div class="nav__top">
-      <a href=""><i class="nav__logo-icon fa-solid fa-scissors"></i></a>
-      <i class="nav__menu-icon fa-solid fa-bars"></i>
-    </div>
-
-    <ul>
-      <li>
-        <a href="" class="nav__link"><i class="fa-solid fa-house"></i>Dashboard</a>
-      </li>
-      <li>
-        <a href="" class="nav__link"><i class="fa-solid fa-calendar-days"></i>Agenda</a>
-      </li>
-      <li>
-        <a href="" class="nav__link"><i class="fa-solid fa-user-group"></i>Clientes</a>
-      </li>
-      <li>
-        <a href="" class="nav__link"><i class="fa-solid fa-handshake"></i>Colaboradores</a>
-      </li>
-    </ul>
-
-    <a href="" class="nav__logout"><i class="fa-solid fa-right-from-bracket"></i>Sair</a>
-  </nav>
+  <?php include dirname(__DIR__) . "/view/components/nav.php"; ?>
 
 </body>
 </html>
