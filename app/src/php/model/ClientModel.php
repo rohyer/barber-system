df4adc13f0e81d73c4acc82760052710eeef47a3
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-09 00:24:41 -0300

    Develop validation to create client register

--- app/src/php/model/ClientModel.php
+++ app/src/php/model/ClientModel.php
@@ -14,33 +14,75 @@ class ClientModel {
     $this->objConnection = new ConnectionModel();
   }
 
+  private function testInput($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  private function validateForm($data) {
+    $nameError = $addressError = $sexError = $birthError = $phoneError = "";
+
+    if (empty($data["name"])) {
+      $nameError = "Campo nome necessário";
+    } else {
+      $this->name = $this->testInput($data["name"]);
+    }
+    if (empty($data["sex"])) {
+      $sexError = "Campo sexo necessário";
+    } else {
+      $this->sex = $this->testInput($data["sex"]);
+    }
+    if (empty($data["address"])) {
+      $addressError = "Campo endereço necessário";
+    } else {
+      $this->address = $this->testInput($data["address"]);
+    }
+    if (empty($data["birth"])) {
+      $birthError = "Campo data de nascimento necessário";
+    } else {
+      $this->birth = $this->testInput($data["birth"]);
+    }
+    if (empty($data["phone"])) {
+      $phoneError = "Campo telefone necessário";
+    } else {
+      $this->phone = $this->testInput($data["phone"]);
+    }
+
+    if ($nameError || $addressError || $sexError || $birthError || $phoneError) {
+      return ["name" => $nameError, "address" => $addressError, "sex" => $sexError, "birth" => $birthError, "phone" => $phoneError];
+    } else {
+      return true;
+    }
+  }
+
   public function createClient($data) {
-    $getConnection = $this->objConnection->getConnection();
+    $validateResult = $this->validateForm($data);
+    
+    if ($validateResult !== true) {
+      return $validateResult;
+    } else {
+      $getConnection = $this->objConnection->getConnection();
 
-    $this->name = $data["name"];
-    $this->address = $data["address"];
-    $this->sex = $data["sex"];
-    $this->birth = $data["birth"];
-    $this->phone = $data["phone"];
+      try {
+        $sql = "INSERT INTO client (name, address, sex, birth, phone) values (:name, :address, :sex, :birth, :phone)";
 
-    try {
-      $sql = "INSERT INTO client (name, address, sex, birth, phone) values (:name, :address, :sex, :birth, :phone)";
+        $stmt = $getConnection->prepare($sql);
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":address", $this->address);
+        $stmt->bindParam(":sex", $this->sex);
+        $stmt->bindParam(":birth", $this->birth);
+        $stmt->bindParam(":phone", $this->phone);
 
-      $stmt = $getConnection->prepare($sql);
-      $stmt->bindParam(":name", $this->name);
-      $stmt->bindParam(":address", $this->address);
-      $stmt->bindParam(":sex", $this->sex);
-      $stmt->bindParam(":birth", $this->birth);
-      $stmt->bindParam(":phone", $this->phone);
-
-      if ($stmt->execute()) {
-        return true;
-      } else {
-        return false;
+        if ($stmt->execute()) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch(PDOException $error) {
+        echo "Error: " . $error->getMessage();
       }
-
-    } catch(PDOException $error) {
-      echo "Error: " . $error->getMessage();
     }
   }
 
@@ -48,7 +90,7 @@ class ClientModel {
     $getConnection = $this->objConnection->getConnection();
 
     try {
-      $sql = "Select name, birth, sex FROM client";
+      $sql = "Select id, name, birth, sex FROM client";
 
       $stmt = $getConnection->prepare($sql);
 
@@ -56,6 +98,28 @@ class ClientModel {
         $result = $stmt->fetchAll();
         return $result;
       }
+    } catch(PDOException $error) {
+      echo "Error: " . $error->getMessage();
+    }
+  }
+
+  public function deleteClient($id) {
+    $getConnection = $this->objConnection->getConnection();
+
+    $this->id = $id;
+
+    try {
+      $sql = "DELETE FROM client WHERE id = :id";
+
+      $stmt = $getConnection->prepare($sql);
+      $stmt->bindParam(":id", $this->id);
+
+      if ($stmt->execute()) {
+        return true;
+      } else {
+        return false;
+      }
+
     } catch(PDOException $error) {
       echo "Error: " . $error->getMessage();
     }
