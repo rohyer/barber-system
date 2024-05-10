488aba83f3840215826aabe70efa86f06ebbabbe
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-10 18:40:08 -0300

    Change to require_once

--- app/src/php/model/EmployeeModel.php
+++ app/src/php/model/EmployeeModel.php
@@ -1,6 +1,7 @@
 <?php
 
-class EmployeeModel {
+class EmployeeModel
+{
   private $objConnection;
   private $id;
   private $name;
@@ -10,26 +11,27 @@ class EmployeeModel {
   private $phone;
   private $salary;
 
-  public function __construct() {
-    include dirname(__DIR__) . "/model/ConnectionModel.php";
+  public function __construct()
+  {
+    require_once dirname(__DIR__) . "/model/ConnectionModel.php";
     $this->objConnection = new ConnectionModel();
   }
 
-  public function read() {
+  public function read()
+  {
     $getConnection = $this->objConnection->getConnection();
 
     try {
       $sql = "Select name from employee";
 
       $stmt = $getConnection->prepare($sql);
-      
+
       if ($stmt->execute()) {
         $result = $stmt->fetchAll();
         return $result;
       }
-    } catch(PDOException $error) {
+    } catch (PDOException $error) {
       echo "Error: " . $error->getMessage();
     }
   }
-
 }
