e2b466df76d206740b76a178863425c262c78321
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-14 21:39:35 -0300

    Develop EmployeeListView

--- app/src/php/view/EmployeeListView.php
+++ app/src/php/view/EmployeeListView.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,11 +11,59 @@
   <script src="https://kit.fontawesome.com/5d76c62972.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="../dist/css/style.min.css">
 </head>
+
 <body>
-  
+
   <?php include dirname(__DIR__) . "/view/components/nav.php"; ?>
 
-  <?php var_dump($result); ?>
+  <div class="client__content">
+    <div class="client__top">
+      <h1 class="client__title">Colaboradores</h1>
+      <a href="colaborador/cadastro"><i class="fa-solid fa-plus"></i></a>
+    </div>
 
+    <div class="client__data">
+      <div class="client__options" style="display: none">
+        <span class="client__categories" data-state="true">Ativos</span>
+        <span class="client__categories" data-state="false">Desativos</span>
+      </div>
+
+      <div class="client__list">
+        <div class="client__head-row">
+          <span>Nome</span>
+          <span>Contato</span>
+          <span>Idade</span>
+          <span>Salário</span>
+        </div>
+
+        <?php
+        foreach ($result as $r) :
+          $date = new DateTime($r["birth"]);
+          $now = new DateTime();
+          $age = $now->diff($date);
+        ?>
+
+          <div class='client__row'>
+            <a href='#'></a>
+            <span><?php echo $r["name"] ?></span>
+            <span><?php echo $r["phone"] ?></span>
+            <span><?php echo $age->y ?></span>
+            <span><?php echo $r["sex"] ?> </span>
+            <span class="client__edit">
+              <a href="colaborador/edita?id=<?php echo $r["id"] ?>">
+                <i class="fa-solid fa-pen-to-square"></i>
+              </a>
+            </span>
+            <span class="client__delete">
+              <a href="colaborador/deleta?id=<?php echo $r["id"] ?>">
+                <i class='fa-solid fa-trash'></i>
+              </a>
+            </span>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  </div>
 </body>
+
 </html>
