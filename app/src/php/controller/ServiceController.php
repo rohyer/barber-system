0fe6d9a3f79aa0b0cbcfcfe069906fa47121622e
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-05-23 22:37:52 -0300

    Develop editService

--- app/src/php/controller/ServiceController.php
+++ app/src/php/controller/ServiceController.php
@@ -6,16 +6,19 @@ use Guilherme\Barbersystem\model\ServiceModel;
 
 require_once $_SERVER["DOCUMENT_ROOT"] . "/barbersystem/app/vendor/autoload.php";
 
-class ServiceController {
+class ServiceController
+{
 
-  public static function readService() {
+  public static function readService()
+  {
     $objServiceModel = new ServiceModel();
     $result = $objServiceModel->read();
 
     require_once dirname(__DIR__) . "/view/ServiceListView.php";
   }
 
-  public static function createService() {
+  public static function createService()
+  {
     $objServiceModel = new ServiceModel();
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -32,12 +35,27 @@ class ServiceController {
     require_once dirname(__DIR__) . "/view/ServiceCreateFormView.php";
   }
 
-  public static function deleteService($id) {
+  public static function deleteService($id)
+  {
     $objServiceModel = new ServiceModel();
     $result = $objServiceModel->delete($id);
 
     if ($result) {
       header("Location: /barbersystem/app/public/servicos");
     }
+  }
+
+  public static function editService($id)
+  {
+    $objServiceModel = new ServiceModel();
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $objServiceModel->edit($_POST);
+      $result = $objServiceModel->getServiceToEdit($id);
+    } else {
+      $result = $objServiceModel->getServiceToEdit($id);
+    }
+
+    require_once dirname(__DIR__) . "/view/ServiceEditFormView.php";
   }
 }
