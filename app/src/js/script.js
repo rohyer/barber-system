99cfc4e8cae5d792c5ecd847cb0d75d0ab87f956
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-06-04 22:10:07 -0300

    Comment js code to keep state of nav

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", getStateMenu);
+// document.addEventListener("DOMContentLoaded", getStateMenu);
 
-function getStateMenu() {
-  const menuState = sessionStorage.getItem("menu");
+// function getStateMenu() {
+//   const menuState = sessionStorage.getItem("menu");
 
-  if (menuState && menuState === "open") {
-    const nav = document.querySelector("nav.nav");
+//   if (menuState && menuState === "open") {
+//     const nav = document.querySelector("nav.nav");
 
-    nav.setAttribute("data-state", "open");
-  }
-}
+//     nav.setAttribute("data-state", "open");
+//   }
+// }
 
-const navMenu = document.getElementById("nav-menu");
+// const navMenu = document.getElementById("nav-menu");
 
-navMenu.addEventListener("click", changeMenuState);
+// navMenu.addEventListener("click", changeMenuState);
 
-function changeMenuState() {
-  const nav = document.querySelector("nav.nav");
-  const navAttribute = nav.getAttribute("data-state");
+// function changeMenuState() {
+//   const nav = document.querySelector("nav.nav");
+//   const navAttribute = nav.getAttribute("data-state");
 
-  if (navAttribute === "open") {
-    nav.setAttribute("data-state", "close");
-    sessionStorage.setItem("menu", "close");
-  } else if (navAttribute === "close") {
-    nav.setAttribute("data-state", "open");
-    sessionStorage.setItem("menu", "open");
-  }
-}
+//   if (navAttribute === "open") {
+//     nav.setAttribute("data-state", "close");
+//     sessionStorage.setItem("menu", "close");
+//   } else if (navAttribute === "close") {
+//     nav.setAttribute("data-state", "open");
+//     sessionStorage.setItem("menu", "open");
+//   }
+// }
