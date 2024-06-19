367a54b3875c3d84ebe638ef91b2198ad921fa0c
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-06-18 21:04:46 -0300

    Change sql to get employee name

--- app/src/php/model/CustomerServiceModel.php
+++ app/src/php/model/CustomerServiceModel.php
@@ -102,7 +102,7 @@ class CustomerServiceModel
     $getConnection = $this->objConnection->getConnection();
 
     try {
-      $sql = "SELECT cs.id, cs.date, cs.time, c.phone, c.name as client, s.name as service FROM customer_service cs JOIN client c ON cs.id_client = c.id JOIN service s ON cs.id_service = s.id ORDER BY cs.date, cs.time";
+      $sql = "SELECT cs.id, cs.date, cs.time, c.phone, c.name as client, s.name as service, e.name as employee FROM customer_service cs JOIN client c ON cs.id_client = c.id JOIN service s ON cs.id_service = s.id JOIN employee e ON cs.id_employee = e.id ORDER BY cs.date, cs.time";
 
       $stmt = $getConnection->prepare($sql);
 
