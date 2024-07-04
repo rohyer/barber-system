13e7af229d04d460e66b828cd4ff0ddcb19cac69
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-07-04 00:03:45 -0300

    Fix variable names

--- app/src/php/view/AdminHomeView.php
+++ app/src/php/view/AdminHomeView.php
@@ -27,10 +27,10 @@
         <div class="home__card">
           <h3 class="home__card-title">Dados do mês atual</h3>
 
-          <p class="home__card-text">Atendimentos feitos: <?php echo $amountClosedCustomerService[0]["amount"]; ?></p>
-          <p class="home__card-text">Previsão de atendimentos: <?php echo $amountOpenCustomerService[0]["amount"]; ?></p>
-          <p class="home__card-text">Faturamento: R$ <?php echo $closedValue[0]['value']; ?></p>
-          <p class="home__card-text">Valor a entrar: R$ <?php echo $openValue[0]['value']; ?></p>
+          <p class="home__card-text">Atendimentos feitos: <?php echo $amountClosedData[0]["amount"]; ?></p>
+          <p class="home__card-text">Previsão de atendimentos: <?php echo $amountOpenData[0]["amount"]; ?></p>
+          <p class="home__card-text">Faturamento: R$ <?php echo $closedValueSum[0]['value']; ?></p>
+          <p class="home__card-text">Valor a entrar: R$ <?php echo $openValueSum[0]['value']; ?></p>
         </div>
         <div class="home__card">
           <h3 class="home__card-title">Atendimentos por serviços</h3>
