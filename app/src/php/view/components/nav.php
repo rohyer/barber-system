3463210505a6f834eec5cc3024f1c984f012e46b
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-08-22 22:09:07 -0300

    Fix nav to width with large height

--- app/src/php/view/components/nav.php
+++ app/src/php/view/components/nav.php
@@ -1,41 +1,43 @@
 <nav class="nav" data-state="open">
-  <div class="nav__top">
-    <!-- <a href=""><i class="nav__logo-icon fa-solid fa-scissors"></i></a> -->
-    <div><i class="nav__menu-icon fa-solid fa-bars" id="nav-menu"></i></div>
-  </div>
+  <div class="nav__box">
+    <div class="nav__top">
+      <!-- <a href=""><i class="nav__logo-icon fa-solid fa-scissors"></i></a> -->
+      <div><i class="nav__menu-icon fa-solid fa-bars" id="nav-menu"></i></div>
+    </div>
 
-  <ul>
-    <li>
-      <a href="/barbersystem/app/public" class="nav__link">
-        <div><i class="fa-solid fa-house"></i></div>Dashboard
-      </a>
-    </li>
-    <li>
-      <a href="/barbersystem/app/public/agenda" class="nav__link">
-        <div><i class="fa-solid fa-calendar-days"></i></div>Agenda
-      </a>
-    </li>
-    <li>
-      <a href="/barbersystem/app/public/clientes" class="nav__link">
-        <div><i class="fa-solid fa-user-group"></i></div>Clientes
-      </a>
-    </li>
-    <li>
-      <a href="/barbersystem/app/public/colaboradores" class="nav__link">
-        <div><i class="fa-solid fa-handshake"></i></div>Colaboradores
-      </a>
-    </li>
-    <li>
-      <a href="/barbersystem/app/public/servicos" class="nav__link">
-        <div><i class="fa-solid fa-scissors"></i></div>Serviços
-      </a>
-    </li>
-  </ul>
+    <ul>
+      <li>
+        <a href="/barbersystem/app/public" class="nav__link">
+          <div><i class="fa-solid fa-house"></i></div>Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="/barbersystem/app/public/agenda" class="nav__link">
+          <div><i class="fa-solid fa-calendar-days"></i></div>Agenda
+        </a>
+      </li>
+      <li>
+        <a href="/barbersystem/app/public/clientes" class="nav__link">
+          <div><i class="fa-solid fa-user-group"></i></div>Clientes
+        </a>
+      </li>
+      <li>
+        <a href="/barbersystem/app/public/colaboradores" class="nav__link">
+          <div><i class="fa-solid fa-handshake"></i></div>Colaboradores
+        </a>
+      </li>
+      <li>
+        <a href="/barbersystem/app/public/servicos" class="nav__link">
+          <div><i class="fa-solid fa-scissors"></i></div>Serviços
+        </a>
+      </li>
+    </ul>
 
-  <div class="nav__logout">
-    <a href="">
-      <div><i class="fa-solid fa-right-from-bracket"></i></div>Sair
-    </a>
+    <div class="nav__logout">
+      <a href="">
+        <div><i class="fa-solid fa-right-from-bracket"></i></div>Sair
+      </a>
+    </div>
   </div>
 </nav>
 
